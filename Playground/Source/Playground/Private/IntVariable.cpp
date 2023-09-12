47bede8bbba373b7a3a16f9f75ca5ffa181d88e0
DistractionCrab <6255349+DistractionCrab@users.noreply.github.com>
2023-09-12 16:01:26 -0400

    Multiple Updates
    Observable Variables:
    - Convert IntVariable to ActorComponent to facility defaulting and reflecting that the variable is innate to actors when available.
    
    Health Bars:
    - Setup Health listening for the Main Character.  Soon Entity health bars will be implemented.
    
    Materials
    - WaterPlane MAterial updated for adjustment of Amplitude.
    - World aligned material made for environment and created as DefaultMaterial
    
    Entities:
    - Created WaterHuman as a simple entity utilizing the Water Material for a fun effect.
    
    Meshes:
    - Remade UV's for SpiralTower. HAs an issue still on one area near the elevator (To Be Fixed)

--- Playground/Source/Playground/Private/IntVariable.cpp
+++ Playground/Source/Playground/Private/IntVariable.cpp
@@ -3,3 +3,55 @@
 
 #include "IntVariable.h"
 
+// Sets default values for this component's properties
+UIntVariable::UIntVariable()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UIntVariable::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UIntVariable::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
+
+int UIntVariable::SetValue(int NewValue) {
+	NewValue = FMath::Clamp(NewValue, this->Min, this->Min);
+	for (FIntModifier& f : this->ChangeModifiers) {
+		if (f.IsBound()) {
+			NewValue = FMath::Clamp(f.Execute(this->Value, NewValue, this), this->Min, this->Min);
+		}
+	}
+
+	this->ChangeObserversCollection.Broadcast(this->Value, NewValue, this);
+	this->Value = NewValue;
+	return this->Value;
+}
+
+int UIntVariable::OperateValue(UVariableOperator* Op) {
+	for (FIntOperatorObserver& f : this->ChangeOperators) {
+		f.ExecuteIfBound(this->Value, Op, this);
+	}
+
+
+	return this->SetValue(Op->ApplyInt(this->Value));
+}
