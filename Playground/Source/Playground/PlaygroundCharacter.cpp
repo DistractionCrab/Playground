69561ce71f9f89afbc2f1cee354be55a79dad2e2
DistractionCrab <6255349+DistractionCrab@users.noreply.github.com>
2023-09-26 09:12:40 -0400

    Some Minor work
    Did some testing with projectile implementations; Need to rework spells a bit to make it work, and that'll be done tomorrow.
    
    Lock-on camera has been fixed and works quite seamlessly now.
    
    Added some more structures to the village, and soon caves will be implemented.

--- Playground/Source/Playground/PlaygroundCharacter.cpp
+++ Playground/Source/Playground/PlaygroundCharacter.cpp
@@ -61,7 +61,7 @@ APlaygroundCharacter::APlaygroundCharacter()
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
 	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.01f;
+	GetCharacterMovement()->AirControl = 0.2f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
